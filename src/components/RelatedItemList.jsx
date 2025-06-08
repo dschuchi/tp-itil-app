791909369a40d6a3637e46eec9ec400f3201dd0e
sschuchinsky <s.schuchinsky@gmail.com>
2025-06-08 20:39:32 -0300

    Formatea el componente RelatedItemList para mejorar la legibilidad del código.

--- src/components/RelatedItemList.jsx
+++ src/components/RelatedItemList.jsx
@@ -3,23 +3,23 @@ import { CloseCircleOutlined, LinkOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const RelatedItemList = ({ data, header, basePath, onDelete }) => (
-  <List
-    dataSource={data}
-    header={<strong>{header}</strong>}
-    renderItem={item => (
-      <List.Item>
-        <Space>
-          <Button onClick={() => onDelete(item.id)}>
-            <CloseCircleOutlined />
-          </Button>
-          <Link to={`${basePath}/${item.id}`}>
-            <LinkOutlined /> {item.id}
-          </Link>
-          <Typography.Text>- {item.title}</Typography.Text>
-        </Space>
-      </List.Item>
-    )}
-  />
+    <List
+        dataSource={data}
+        header={<strong>{header}</strong>}
+        renderItem={item => (
+            <List.Item>
+                <Space>
+                    <Button onClick={() => onDelete(item.id)}>
+                        <CloseCircleOutlined />
+                    </Button>
+                    <Link to={`${basePath}/${item.id}`}>
+                        <LinkOutlined /> {item.id}
+                    </Link>
+                    <Typography.Text>- {item.title}</Typography.Text>
+                </Space>
+            </List.Item>
+        )}
+    />
 );
 
 export default RelatedItemList;
